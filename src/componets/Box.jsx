b24d9c2c9d6619ab1680d1665b61ea6a0dcfbf0e
Temurbek Sayfutdinov <temursayfutdinov3@gmail.com>
2023-02-26 05:50:53 -0500

    third change

--- src/componets/Box.jsx
+++ src/componets/Box.jsx
@@ -11,19 +11,19 @@ const Box = (props) => {
             <td>
                 <img src = {Lcfc} />
                  <div className = "info">
-                    <td><Fixture date = "Feb/25" homeTeam = "Leicester City" awayTeam = "Arsenal" stadium = "King Power Stadium" time = "10:00AM"/></td>
+                    <td><Fixture link = "https://www.arsenal.com/fixture/arsenal/2023-Feb-25/leicester-city-0-1-arsenal-match-report" date = "Feb/25" homeTeam = "Leicester City" awayTeam = "Arsenal" stadium = "King Power Stadium" time = "10:00AM"/></td>
                 </div>
             </td>
             <td>
                 <img src = {Everton} />
                 <div className="info">
-                    <td><Fixture date = "March/1" homeTeam = "Arsenal" awayTeam = "Everton" stadium = "Emirates Stadium" time = "11:30AM"/></td>
+                    <td><Fixture link = "https://www.premierleague.com/match/74971" date = "March/1" homeTeam = "Arsenal" awayTeam = "Everton" stadium = "Emirates Stadium" time = "11:30AM"/></td>
                 </div>
             </td>
             <td>
                <img src = {Sporting} />
                 <div className = "info">
-                    <td><Fixture date = "March/9" homeTeam = "Sporting Lisbon" awayTeam = "Arsenal" stadium = "Estádio José Alvalade" time = "12:45PM"/></td>
+                    <td><Fixture link = "https://www.arsenal.com/news/sporting-cp-drawn-europa-league-last-16" date = "March/9" homeTeam = "Sporting Lisbon" awayTeam = "Arsenal" stadium = "Estádio José Alvalade" time = "12:45PM"/></td>
                 </div>
             </td>
             </tr>
--- src/componets/Fixture.jsx
+++ src/componets/Fixture.jsx
@@ -6,7 +6,9 @@ const Fixture = (props) => {
             <h5>{props.date}</h5>
             <h5>{props.homeTeam} V {props.awayTeam}</h5>
             <h6>{props.stadium} {props.time}</h6>
-            <button className = "stream">Stream Game</button>
+            <a href = {props.link} target = "_blank">
+            <button className = "stream">View Details</button>
+            </a>
          </td>
     )
 }

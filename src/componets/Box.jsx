c571dbe9371686219cdca6d39d2c147413437a7d
Temurbek Sayfutdinov <temursayfutdinov3@gmail.com>
2023-02-28 13:25:33 -0500

    Final Change

--- src/componets/Box.jsx
+++ src/componets/Box.jsx
@@ -3,7 +3,12 @@ import Fixture from "./Fixture";
 import Lcfc from "../images/LCFCVAR.jpeg"
 import Everton from "../images/ArsenalVEverton.jpeg"
 import Sporting from "../images/Sporting.jpeg"
-const Box = (props) => {
+import AFCB from "../images/AFCB.jpeg"
+import Fulham from "../images/Fulham.jpeg"
+import CPFC from "../images/CPFC.jpeg"
+import LUFC from "../images/LUFC.jpeg"
+import LFC from "../images/LFC.jpeg"
+const Box = () =>{
     return (
         <table className = "Boxs">
         <thead>
@@ -21,13 +26,55 @@ const Box = (props) => {
                 </div>
             </td>
             <td>
-               <img src = {Sporting} />
+                <img src = {AFCB} />
                 <div className = "info">
-                    <td><Fixture link = "https://www.arsenal.com/news/sporting-cp-drawn-europa-league-last-16" date = "March/9" homeTeam = "Sporting Lisbon" awayTeam = "Arsenal" stadium = "Estádio José Alvalade" time = "12:45PM"/></td>
+                    <td><Fixture link = "https://www.premierleague.com/match/75161" date = "March/4" homeTeam = "Arsenal" awayTeam = "AFC Bournmouth" stadium = "Emirates Stadium" time = "10:00AM"/></td>
                 </div>
             </td>
             </tr>
         </thead>
+        <tbody>
+            <tr>
+            <td>
+            <img src = {Sporting} />
+                <div className = "info">
+                    <td><Fixture link = "https://www.arsenal.com/news/sporting-cp-drawn-europa-league-last-16" date = "March/9" homeTeam = "Sporting Lisbon" awayTeam = "Arsenal" stadium = "Estádio José Alvalade" time = "12:45PM"/></td>
+                </div>
+            </td>
+            <td>
+                <img src = {Fulham} />
+                <div className = "info">
+                <td><Fixture link = "https://allnews.ng/news/fulham-vs-arsenal-preview-team-news-kick-off-time-where-to-watch-epl-match-live" date = "March/12" time = "10:00AM" homeTeam = "Fulham" awayTeam = "Arsenal" stadium = "Craven Cottage"/></td>
+                </div>
+            </td>
+            <td>
+                <img src = {Sporting}/>
+                <div className = "info">
+                    <td><Fixture link = "https://www.bbc.com/sport/articles/c4nqr1q1vq9o" date = "March/16" time = "4:00PM" homeTeam = "Arsenal" awayTeam = "Sporting Lisobon" stadium = "Emirates Stadium"/></td>
+                </div>
+            </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src = {CPFC} />
+                    <div className = "info">
+                        <td><Fixture link = "https://www.espn.com/soccer/match/_/gameId/638089" date = "March/19" time = "10:00AM" homeTeam = "Arsenal" awayTeam = "Crystal Palace" stadium = "Emirates Stadium"/></td>
+                    </div>
+                </td>
+                <td>
+                    <img src = {LUFC} />
+                    <div className = "info">
+                        <td><Fixture link = "https://www.sofascore.com/arsenal-leeds-united/JsR" date = "April/1" time = "10:00AM" homeTeam = "Arsenal" awayTeam = "Leeds United" stadium = "Emirates Stadium"/></td>
+                    </div>
+                </td>
+                <td>
+                    <img src = {LFC} />
+                    <div className="info">
+                        <td><Fixture link = "https://www.sofascore.com/liverpool-arsenal/HQfh" date = "April/9" time = "11:30AM" homeTeam = "Liverpool" awayTeam = "Arsenal" stadium = "Anfield"/></td>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
         </table>
     )
 }
